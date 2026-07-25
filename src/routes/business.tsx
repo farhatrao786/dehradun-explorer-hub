8b9b82e77fd0ccf9c34459facc9bfc9aa62d3f61
farhatrao786 <farhat.rao@gmail.com>
2026-07-25 18:51:09 +0530

    Update business.tsx

--- src/routes/business.tsx
+++ src/routes/business.tsx
@@ -1,1 +1,14 @@
+import { createFileRoute } from "@tanstack/react-router";
 
+export const Route = createFileRoute("/business")({
+  component: BusinessPage,
+});
+
+function BusinessPage() {
+  return (
+    <div>
+      <h1>List Your Business</h1>
+      <p>Welcome to TheDehradun.com Business Listing</p>
+    </div>
+  );
+}
